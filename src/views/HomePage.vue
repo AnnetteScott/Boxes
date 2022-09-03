1aefed8cf8aa51619a7f882eed352fa94da1c085
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2022-09-03 19:55:43 +1200

    init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+    <ion-page>    
+        <ion-content :fullscreen="true">
+
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonContent, IonPage } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'HomePage',
+    components: {
+        IonContent,
+        IonPage,
+    }
+});
+</script>
+
+<style>
+
+</style>
